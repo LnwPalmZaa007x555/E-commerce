e12e4a962fe04382bca7c05ac97043afd6420ecf
palm <palmpalm1407@gmail.com>
2025-09-27 17:43:09 +0700

    edit new middleware

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,12 @@
-// middleware.ts
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware } from '@clerk/nextjs/server'
 
-export default clerkMiddleware();
+export default clerkMiddleware()
 
-// ตัด /api/inngest ออกด้วย Negative Lookahead
 export const config = {
   matcher: [
-    "/((?!.+\\.[\\w]+$|_next).*)",
-    "/",
-    "/(api)(?!/inngest).*",   // ⬅️ exclude /api/inngest
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
   ],
-};
+}
