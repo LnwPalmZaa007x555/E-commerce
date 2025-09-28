034c3be34b5f9fc16a78b48da5160e795b1a3dfe
palm <palmpalm1407@gmail.com>
2025-09-28 16:14:05 +0700

    edit inngest calculate and item amount

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,10 +1,14 @@
 import { serve } from "inngest/next";
-import { inngest, syncUserCreation, syncUserUpdation, syncUserDeletion } from "@/config/inngest";
+import { inngest, syncUserCreation, syncUserUpdation, syncUserDeletion, createUserOrder } from "@/config/inngest";
 
 export const runtime = "nodejs";
 export const dynamic = "force-dynamic";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [syncUserCreation, syncUserUpdation, syncUserDeletion],
+  functions: [syncUserCreation, 
+    syncUserUpdation, 
+    syncUserDeletion,
+    createUserOrder
+  ],
 });
--- config/inngest.js
+++ config/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import connectDB from "./db";
 import User from "@/models/user";
+import Order from "@/models/order";
 
 // Inngest client
 export const inngest = new Inngest({ id: "QuickCart-next" });
@@ -51,3 +52,33 @@ export const syncUserDeletion = inngest.createFunction(
     await User.findByIdAndDelete(id)
   }
 );
+
+// inngest func to create user order in db
+export const createUserOrder = inngest.createFunction(
+  {
+    id:'create-user-order',
+    batchEvents: {
+      maxSize: 25,
+      timeout: '5s'
+    }
+  },
+  {event: 'order/created'},
+  async ({events}) =>{
+
+    const orders = events.map((event)=>{
+      return{
+        userId: event.data.userId,
+        item: event.data.item,
+        amount: event.data.amount,
+        address: event.data.address,
+        date: event.data.date
+      }
+    })
+
+    await connectDB()
+    await Order.insertMany(orders)
+
+    return{ success:true, process:orders.length}
+
+  }
+)
